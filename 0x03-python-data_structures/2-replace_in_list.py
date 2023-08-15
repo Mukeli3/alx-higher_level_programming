ef43d6573ce77d75a0cf7479b9ba06f9029e52a3
Mukeli3 <mukelicynthia20@gmail.com>
2023-08-15 08:23:42 +0000

    pycodestyle fix

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-#replacing a list element at a specific position like in C
+'''replacing a list element at a specific position like in C'''
 
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
