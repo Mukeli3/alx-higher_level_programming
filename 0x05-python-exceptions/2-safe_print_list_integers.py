b133b84dca9bc78621253057245e38263201a44b
Mukeli3 <mukelicynthia20@gmail.com>
2023-08-28 21:13:23 +0000

    fix attempt

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,5 +9,6 @@ def safe_print_list_integers(my_list=[], x=0):
             print()
             return count
         except (ValueError, TypeError):
-            print()
-            return count
+            continue
+        print()
+        return count
