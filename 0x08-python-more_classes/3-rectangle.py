82c01583ba505805f4231830686e2b0bbf836197
Mukeli3 <mukelicynthia20@gmail.com>
2023-10-02 21:22:04 +0000

    pycodestyle fix

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -39,6 +39,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__height + self.__width)
+
     def __str__(self):
         """ method, print the rectangle with the # character """
         if self.__width == 0 or self.__height == 0:
