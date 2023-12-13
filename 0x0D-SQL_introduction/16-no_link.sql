ec1c26979b0c10b0c529a638f7eede8f2efd501c
Mukeli3 <mukelicynthia20@gmail.com>
2023-12-13 20:18:18 +0000

    filled gap

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists all records of the table second_table of the database hbtn_0c_0 in the MySQL server
-SELECT score, name FROM second_table WHERE name IS NOT NULL != '' ORDER BY 'score' DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL and name != '' ORDER BY 'score' DESC;
