2c808919015bf20bdf6e25a9f71949c4e0c2dfd9
Mukeli3 <mukelicynthia20@gmail.com>
2024-01-09 21:32:01 +0000

    square to square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const olderSquare = require('./5-square');
+
+class Square extends olderSquare {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
